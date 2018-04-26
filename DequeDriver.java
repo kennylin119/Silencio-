5aadf8af87e8ea499778ba091fcb0133286b4b53
Taaseen Ali <taaseen.ali89@gmail.com>
2018-04-25 23:39:13 -0400

    update driver

--- DequeDriver.java
+++ DequeDriver.java
@@ -28,65 +28,36 @@ public class DequeDriver{
 
     System.out.println("-----Adding to front------");
     iscream.addFirst("Chocolate");
-    System.out.println(iscream.peekFirst()); //Chocolate
+    System.out.println(iscream.getFirst()); //Chocolate
     iscream.addFirst("Vanilla");
-    System.out.println(iscream.peekFirst()); //Vanilla
+    System.out.println(iscream.getFirst()); //Vanilla
     iscream.addFirst("Strawberry");
-    System.out.println(iscream.peekFirst()); //Strawberry
+    System.out.println(iscream.getFirst()); //Strawberry
     iscream.addFirst("Mint");
-    System.out.println(iscream.peekFirst()); //Mint
+    System.out.println(iscream.getFirst()); //Mint
 
     System.out.println("");
 
-    System.out.println("------Current Deque------");
-    System.out.println(iscream); //Mint Strawberry Vanilla Chocolate
+    //System.out.println("------Current Deque------");
+    //System.out.println(iscream); //Mint Strawberry Vanilla Chocolate
 
     System.out.println("");
 
     System.out.println("-----Adding to end------");
     iscream.addLast("Pistachio");
-    System.out.println(iscream.peekLast()); //Pistachio
+    System.out.println(iscream.getLast()); //Pistachio
     iscream.addLast("Coffee");
-    System.out.println(iscream.peekLast()); //Coffee
+    System.out.println(iscream.getLast()); //Coffee
     iscream.addLast("S'mores");
-    System.out.println(iscream.peekLast()); //S'mores
-
-    System.out.println("");
-
-    System.out.println("------Current Deque------");
-    System.out.println(iscream); //Mint Strawberry Vanilla Chocolate Pistachio Coffee S'mores
-
-    System.out.println("");
+    System.out.println(iscream.getLast()); //S'mores
 
     System.out.println("-----Removing from front-----");
-    iscream.pollFirst(); //Mint  dies
-    System.out.println(iscream.peekFirst()); //Strawberry
+    System.out.println(iscream.getFirst()); //Mint
 
     System.out.println("");
 
     System.out.println("-----Removing from end-----");
-    iscream.pollLast(); //S'mores dies
-    System.out.println(iscream.peekLast()); //Coffee
-
-    System.out.println("");
-
-    System.out.println("------Current Deque------");
-    System.out.println(iscream); //Mint Strawberry Vanilla Chocolate Pistachio Coffee S'mores
-
-    System.out.println("");
-
-    System.out.println("------REMOVING EVERYTHING------");
-    iscream.pollFirst();
-    System.out.println(iscream);
-    iscream.pollLast();
-    System.out.println(iscream);
-    iscream.pollFirst();
-    System.out.println(iscream);
-    iscream.pollLast();
-    System.out.println(iscream);
-    iscream.pollFirst();
-    System.out.println(iscream);
-    System.out.println("Empty now?: "+iscream.isEmpty()); //true
+    System.out.println(iscream.getLast()); //S'mores
   }
 
 
